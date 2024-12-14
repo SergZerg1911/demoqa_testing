d80769572ec6eeb5af6f20870c8c93ed40578dfc
sergzerg1911 <sergzerg1911@gmail.com>
2024-12-14 14:59:48 +0300

    added new tests

--- src/test/java/pages/TextBoxPage.java
+++ src/test/java/pages/TextBoxPage.java
@@ -2,7 +2,7 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -14,6 +14,7 @@ public class TextBoxPage {
                     currentAddress = $("#currentAddress"),
                     permanentAddress = $("#permanentAddress"),
                     submit = $("#submit"),
+                    outputField = $("#output"),
                     outputUserNameCheck = $("#output").$("#name"),
                     outputUserEmailCheck = $("#output").$("#email"),
                     outputCurrentAddressCheck = $("#output").$("#currentAddress"),
@@ -68,4 +69,29 @@ public class TextBoxPage {
         outputPermanentAddressCheck.shouldHave(text(value));
         return this;
     };
+
+    public TextBoxPage verifyUserNameIsEmpty(){
+        outputField.shouldNotHave(text("Name:"));
+        return this;
+    }
+
+    public TextBoxPage verifyCurrentAddressIsEmpty(){
+        outputCurrentAddressCheck.shouldNotHave(text("Current Address :"));
+        return this;
+    }
+
+    public TextBoxPage verifyPermanentAddressIsEmpty(){
+        outputPermanentAddressCheck.shouldNotHave(text("Permananet Address :"));
+        return this;
+    }
+
+    public TextBoxPage verifyOutputFieldIsEmpty(){
+        outputField.shouldBe(empty);
+        return this;
+    }
+
+    public TextBoxPage verifyUserEmailBorderIsRed(){
+        userEmail.shouldHave(cssValue("color", "rgba(73, 80, 87, 1)"));
+        return this;
+    }
 }
--- src/test/java/tests/TextBoxTest.java
+++ src/test/java/tests/TextBoxTest.java
@@ -6,11 +6,12 @@ public class TextBoxTest extends BaseConfig{
 
     private String userName = "Scott L Jones";
     private String userEmail = "s.k.i.l.ig.ge.e@gmail.com";
+    private String invalidUserEmail = "s.k.i.l.ig.ge.e";
     private String currentAddress = "4176 Rinehart Road Miami, Florida(FL), 33179";
     private String permanentAddress = "65 Maxwell Street Hartford, Connecticut(CT), 06103";
 
     @Test
-    void fillTheFormTest(){
+    void completelyFillOutTheForm(){
         textBoxPage.openPage()
                 .setUserName(userName)
                 .setUserEmail(userEmail)
@@ -21,5 +22,30 @@ public class TextBoxTest extends BaseConfig{
                 .verifyUserEmail(userEmail)
                 .verifyCurrentAddress(currentAddress)
                 .verifyPermanentAddress(permanentAddress);
+    }
+
+    @Test
+    void FillOutTheFormWithoutName(){
+        textBoxPage.openPage()
+                .setUserEmail(userEmail)
+                .setCurrentAddress(currentAddress)
+                .setPermanentAddress(permanentAddress)
+                .clickSubmit()
+                .verifyUserNameIsEmpty()
+                .verifyUserEmail(userEmail)
+                .verifyCurrentAddress(currentAddress)
+                .verifyPermanentAddress(permanentAddress);
+    }
+
+    @Test
+    void FillOutTheFormWithInvalidUserEmail(){
+        textBoxPage.openPage()
+                .setUserName(userName)
+                .setUserEmail(invalidUserEmail)
+                .setCurrentAddress(currentAddress)
+                .setPermanentAddress(permanentAddress)
+                .clickSubmit()
+                .verifyUserEmailBorderIsRed()
+                .verifyOutputFieldIsEmpty();
     }
 }
